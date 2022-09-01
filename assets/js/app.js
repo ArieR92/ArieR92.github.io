a250560f868171633968ca878c9bb1d1c07a4d22
Ariel <arielrodriguez0@outlook.com>
2022-09-01 14:25:37 -0600

    commit-2

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ window.onscroll = function() {
     if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0 ) {
         document.getElementById('navbar').classList.add('scrolled');
     } else {
-        document.getElementById('navbar').classList.remove('scrolled');
+        document.getElementById('navbar').classList.add('scrolled');
     }
 }
 
